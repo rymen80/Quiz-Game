dd39e75ca09ee5be487ab6e19519d83e0876a48b
Ryan Mendez <rjmendez80@gmail.com>
2020-08-02 21:09:34 -0700

    in js start button and timer working, added most html tags

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-const $timer = document.querySelector('timer');
+// an array of objects coinating the questions, possible answers and correct answer.
+const questions = [
+    {   Question: "Question 1: Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        Question: "Question 2: The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        Question: "Question 3: The instructions for a function are enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "curly brackets"
+    },
+    {
+        Question: "Question 4: A property of an object that is a function is called a ____.",
+        choices: ["method", "string", "stylesheet", "boolean"],
+        answer: "method"
+    },
+    {
+        Questions: "Question 5: The logical operator that represents 'or' is ____.",
+        choices: ["||", "OR", "&&", "==="],
+        answer: "||"
+    }
+];
 
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
+const $timerTextArea = document.querySelector("#timer-text-area");
+const $highscoreButton = document.querySelector("#highscore-button");
+const $btnStart = document.querySelector(".btnStart");
+const $jumboHeader = document.querySelector(".jumbo-header");
+const $questionContainer = document.querySelector(".question-container");
+const $questionArea = document.querySelector(".question-area");
+const $buttonArea = document.querySelector(".button-area");
+const $results = document.querySelector(".results");
 
-// THEN a timer starts and I am presented with a question
 // WHEN I answer a question
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
@@ -13,4 +44,38 @@ const $timer = document.querySelector('timer');
 // WHEN the game is over
 // THEN I can save my initials and score
 // ```
-$timer.textContent = 'this is the timer';
+window.onload = function() {
+    $buttonArea.style.visibility= "hidden";
+    $questionArea.textContent = "Click start to begin Quiz"
+    
+}
+$btnStart.addEventListener("click",takeQuiz);
+
+function takeQuiz() {
+    startClock();
+    DisplayQuestions();
+}
+function startClock() {
+    $btnStart.style.visibility = "hidden";
+    $buttonArea.style.visibility = "visible";
+    let timeLeft = 60;
+    const countDownInterval = setInterval(function(){
+    $timerTextArea.value = timeLeft;
+    // timeLeft = timeLeft - 1;
+    timeLeft--;
+    if (timeLeft === 0) {
+    $timerTextArea.value = '';
+    clearInterval(countDownInterval);
+    //more code here for the game.
+    }
+  }, 1000);
+}
+
+function DisplayQuestions() {
+    $jumboHeader.textContent = "Quiz In Progress"
+    questions.forEach((index,element,array) => {
+        $questionArea.textContent = questions[0].Question
+
+});
+
+}
