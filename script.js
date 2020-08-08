3b902ca8c540a683d87842e1517d18b3a1c80547
Ryan Mendez <rjmendez80@gmail.com>
2020-08-07 18:01:18 -0700

    added game over function,

--- script.js
+++ script.js
@@ -20,14 +20,17 @@ const questions = [
         answer: "method"
     },
     {
-        Questions: "Question 5: The logical operator that represents 'or' is ____.",
+        Question: "Question 5: The logical operator that represents 'or' is ____.",
         choices: ["||", "OR", "&&", "==="],
         answer: "||"
     }
-];
+]
 
+
+// declared variables to access the html elements
 const $timerTextArea = document.querySelector("#timer-text-area");
 const $highscoreButton = document.querySelector("#highscore-button");
+const $timeLabel = document.querySelector(".time-label");
 const $btnStart = document.querySelector(".btnStart");
 const $jumboHeader = document.querySelector(".jumbo-header");
 const $questionContainer = document.querySelector(".question-container");
@@ -38,35 +41,31 @@ const $btn1 = document.querySelector(".btn1");
 const $btn2 = document.querySelector(".btn2");
 const $btn3 = document.querySelector(".btn3");
 const $btn4 = document.querySelector(".btn4");
-
+// declare a variable to count through the questions
+let count = 0;
+// declare score variable to track score of user
 let score = 0;
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-// ```
+// variable to store users score history to send to local storage.
+let userScores = [];
+
+// hides the buttons and displays a messgage to begin quiz upon page load 
 window.onload = function() {
     $buttonArea.style.visibility= "hidden";
     $questionArea.textContent = "Click start to begin Quiz"
-    
 }
-$btnStart.addEventListener("click",takeQuiz);
-//event listeners for all possible answer buttons
-// $btn1.addEventListener("click",evaluateAnswer );
-// $btn2.addEventListener("click",evaluateAnswer );
-// $btn3.addEventListener("click",evaluateAnswer );
-// $btn4.addEventListener("click",evaluateAnswer );
 
-let timeLeft = 60;
+$btnStart.addEventListener("click",takeQuiz);
+
+
+// set time left to the amount of questions multiplied ny ten seconds. IE. 50 secs.
+let timeLeft = 2 * (questions.length);
+console.log(timeLeft);
+
 function takeQuiz() {
     startClock();
     DisplayQuestions();
 }
-
+// start the clock function
 function startClock() {
     $btnStart.style.visibility = "hidden";
     $buttonArea.style.visibility = "visible";
@@ -75,17 +74,22 @@ function startClock() {
     $timerTextArea.value = timeLeft;
     // timeLeft = timeLeft - 1;
     timeLeft--;
-    if (timeLeft === 0) {
-    $timerTextArea.value = '';
-    clearInterval(countDownInterval);
-    //more code here for the game.
+    console.log(timeLeft);
+    if (timeLeft <= 0) {
+        timeLeft = 0;    
+        $timerTextArea.value = '--';
+        $timeLabel.textContent = "Times Up!!";
+        
+        gameOver();
+        clearInterval(countDownInterval);
+    
     }
   }, 1000);
 }
-
+// function that loads first set of questions.
 function DisplayQuestions() {
     $jumboHeader.textContent = "Quiz In Progress"
-    questions.forEach((index,element,array) => {
+     
         $questionArea.textContent = questions[0].Question
         $btn1.textContent = questions[0].choices[0]
         $btn2.textContent = questions[0].choices[1]
@@ -93,16 +97,55 @@ function DisplayQuestions() {
         $btn4.textContent = questions[0].choices[3]
         console.log(questions[0].answer);
 
-});
+    
+}
+//event listeners for all possible answer buttons
+$btn1.addEventListener("click",evaluateAnswer);
+$btn2.addEventListener("click",evaluateAnswer);
+$btn3.addEventListener("click",evaluateAnswer);
+$btn4.addEventListener("click",evaluateAnswer);
+
+
+// function that checks the users answer by comparing the event target to the correct answer
+
+function evaluateAnswer() {
+    // setting variable user answer to the event target
+   
+    
+    let userAnswer = event.target.textContent; 
+    console.log(userAnswer);
+    
+        if(userAnswer === questions[count].answer) {
+            score += 5;
+            $results.textContent = "correct, score is: " + score;
+            
+        }
+        else {
+            timeLeft -= 5;
+            $results.textContent = "incorrect, score is: " + score;
+          
+        }
+        count ++;
+
+        if( count > 4) {
+            gameOver();
+        }
+        else {
+            $questionArea.textContent = questions[count].Question
+            $btn1.textContent = questions[count].choices[0]
+            $btn2.textContent = questions[count].choices[1]
+            $btn3.textContent = questions[count].choices[2]
+            $btn4.textContent = questions[count].choices[3]
+        }
+
+}
+// function that runs when quiz is over.
+function gameOver() {
+    timeLeft = 0;
+    $jumboHeader.textContent = "Quiz has ended"
+    $buttonArea.style.visibility = "hidden";
+    $questionArea.style.visibility = "hidden";
+    $timeLabel.textContent = "";
+    $results.textContent = "GAME OVER. \n Your final score is:  " + score;
 }
 
-// questions.forEach((index,element,array) => {    
-//     $questionArea.textContent = questions[0].Question
-//     $btn1.textContent = questions[0].choices[0]
-//     $btn2.textContent = questions[0].choices[1]
-//     $btn3.textContent = questions[0].choices[2]
-//     $btn4.textContent = questions[0].choices[3]
-//     if(event === questions[0].answer) {
-//         $results.textContent = "Correct";
-//     }
-// });
