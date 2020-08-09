a5e40f71ca689955a87d6f74a3900c9f0d21f919
Ryan Mendez <rjmendez80@gmail.com>
2020-08-09 00:00:41 -0700

    everything except local storage

---
+++ assets/README.md
@@ -0,0 +1,9 @@
+Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+![screenshot1](assets/screenshot1.png)
+![screenshot2](assets/screenshot2.png)
+![screenshot3](assets/screenshot3.png)
+
+github pages link: https://rymen80.github.io/Quiz-Game/
+
+github code link: https://github.com/rymen80/Quiz-Game.git
--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 // an array of objects coinating the questions, possible answers and correct answer.
 const questions = [
-    {   Question: "Question 1: Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        Question: "Question 2: The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        Question: "Question 3: The instructions for a function are enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "curly brackets"
-    },
-    {
-        Question: "Question 4: A property of an object that is a function is called a ____.",
-        choices: ["method", "string", "stylesheet", "boolean"],
-        answer: "method"
-    },
-    {
-        Question: "Question 5: The logical operator that represents 'or' is ____.",
-        choices: ["||", "OR", "&&", "==="],
-        answer: "||"
-    }
-]
-
-
+  {
+    Question: "Question 1: Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    Question:
+      "Question 2: The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    Question:
+      "Question 3: The instructions for a function are enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "curly brackets",
+  },
+  {
+    Question:
+      "Question 4: A property of an object that is a function is called a ____.",
+    choices: ["method", "string", "stylesheet", "boolean"],
+    answer: "method",
+  },
+  {
+    Question: "Question 5: The logical operator that represents 'or' is ____.",
+    choices: ["||", "OR", "&&", "==="],
+    answer: "||",
+  },
+];
 // declared variables to access the html elements
 const $timerTextArea = document.querySelector("#timer-text-area");
 const $highscoreButton = document.querySelector("#highscore-button");
@@ -45,117 +47,136 @@ const $btn4 = document.querySelector(".btn4");
 let count = 0;
 // declare score variable to track score of user
 let score = 0;
-// variable to store users score history to send to local storage.
+// variable to store users score.
 let userScores = [];
-
-// hides the buttons and displays a messgage to begin quiz upon page load 
-window.onload = function() {
-    $buttonArea.style.visibility= "hidden";
-    $questionArea.textContent = "Click start to begin Quiz"
-}
-
-$btnStart.addEventListener("click",takeQuiz);
-
-
+// variable to store user initials
+let userInitial = [];
+// a variable to store the users name and score.
+let userTag = [];
 // set time left to the amount of questions multiplied ny ten seconds. IE. 50 secs.
-let timeLeft = 10 * (questions.length);
-console.log(timeLeft);
-
+let timeLeft = 10 * questions.length;
+// hides the buttons and displays a messgage to begin quiz upon page load
+window.onload = function () {
+  $buttonArea.style.visibility = "hidden";
+  $questionArea.textContent = "Click start to begin Quiz";
+};
+$highscoreButton.addEventListener("click", getScores);
+// on click event that loads the take quiz function
+$btnStart.addEventListener("click", takeQuiz);
+// function that runs the startClock and displaysQuestions functions
 function takeQuiz() {
-    startClock();
-    DisplayQuestions();
+  startClock();
+  displayQuestions();
 }
 // start the clock function
 function startClock() {
-    $btnStart.style.visibility = "hidden";
-    $buttonArea.style.visibility = "visible";
-    
-    const countDownInterval = setInterval(function(){
+  $btnStart.style.visibility = "hidden";
+  $buttonArea.style.visibility = "visible";
+  const countDownInterval = setInterval(function () {
     $timerTextArea.value = timeLeft;
     // timeLeft = timeLeft - 1;
     timeLeft--;
     console.log(timeLeft);
     if (timeLeft <= 0) {
-        timeLeft = 0;    
-        $timerTextArea.value = '--';
-        $timeLabel.textContent = "Times Up!!";
-        
-        gameOver();
-        clearInterval(countDownInterval);
-    
+      timeLeft = 0;
+      $timerTextArea.value = "--";
+      $timeLabel.textContent = "Times Up!!";
+
+      gameOver();
+      clearInterval(countDownInterval);
     }
   }, 1000);
 }
 // function that loads first set of questions.
-function DisplayQuestions() {
-    $jumboHeader.textContent = "Quiz In Progress"
-     
-        $questionArea.textContent = questions[0].Question
-        $btn1.textContent = questions[0].choices[0]
-        $btn2.textContent = questions[0].choices[1]
-        $btn3.textContent = questions[0].choices[2]
-        $btn4.textContent = questions[0].choices[3]
-        console.log(questions[0].answer);
+function displayQuestions() {
+  $jumboHeader.textContent = "Quiz In Progress";
 
-    
+  $questionArea.textContent = questions[0].Question;
+  $btn1.textContent = questions[0].choices[0];
+  $btn2.textContent = questions[0].choices[1];
+  $btn3.textContent = questions[0].choices[2];
+  $btn4.textContent = questions[0].choices[3];
+  console.log(questions[0].answer);
 }
 //event listeners for all possible answer buttons
-$btn1.addEventListener("click",evaluateAnswer);
-$btn2.addEventListener("click",evaluateAnswer);
-$btn3.addEventListener("click",evaluateAnswer);
-$btn4.addEventListener("click",evaluateAnswer);
-
-
+$btn1.addEventListener("click", evaluateAnswer);
+$btn2.addEventListener("click", evaluateAnswer);
+$btn3.addEventListener("click", evaluateAnswer);
+$btn4.addEventListener("click", evaluateAnswer);
 // function that checks the users answer by comparing the event target to the correct answer
-
 function evaluateAnswer() {
-    // setting variable user answer to the event target
-   
-    
-    let userAnswer = event.target.textContent; 
-    console.log(userAnswer);
-    
-        if(userAnswer === questions[count].answer) {
-            score += 5;
-            $results.textContent = "correct, score is: " + score;
-            
-        }
-        else {
-            timeLeft -= 5;
-            $results.textContent = "incorrect, score is: " + score;
-          
-        }
-        count ++;
+  // setting variable user answer to the event target
+  let userAnswer = event.target.textContent;
+  console.log(userAnswer);
 
-        if( count > 4) {
-            gameOver();
-        }
-        else {
-            $questionArea.textContent = questions[count].Question
-            $btn1.textContent = questions[count].choices[0]
-            $btn2.textContent = questions[count].choices[1]
-            $btn3.textContent = questions[count].choices[2]
-            $btn4.textContent = questions[count].choices[3]
-        }
-
+  if (userAnswer === questions[count].answer) {
+    score += 5;
+    $results.textContent = "correct, score is: " + score;
+  } else {
+    timeLeft -= 5;
+    $results.textContent = "incorrect, score is: " + score;
+  }
+  count++;
+  if (count > 4) {
+    gameOver();
+  } else {
+    $questionArea.textContent = questions[count].Question;
+    $btn1.textContent = questions[count].choices[0];
+    $btn2.textContent = questions[count].choices[1];
+    $btn3.textContent = questions[count].choices[2];
+    $btn4.textContent = questions[count].choices[3];
+  }
 }
 // function to input user score
 function enterScore() {
-    let scoreInput = document.createElement("textarea");
-    scoreInput.setAttribute("rows",1); 
-    scoreInput.setAttribute("cols",3);
-    $questionContainer.textContent = "You scored " + score + " enter intials to record score";
-    $questionContainer.appendChild(scoreInput);
+  let initialsInput = document.createElement("textarea");
+  initialsInput.setAttribute("rows", 1);
+  initialsInput.setAttribute("cols", 3);
+  initialsInput.style.marginTop = "20px";
+  initialsInput.className += "initials-input";
+  $questionContainer.textContent =
+    "You scored " + score + " enter intials to record score  ";
+  $questionContainer.appendChild(initialsInput);
+  let inputButton = document.createElement("button");
+  inputButton.className += "submit-button";
+  inputButton.textContent = "submit";
+  inputButton.style.marginLeft = "20px";
+  $questionContainer.appendChild(inputButton);
+  const $submitButton = document.querySelector(".submit-button");
+  $submitButton.addEventListener("click", function () {
+    if (initialsInput.value) {
+      userInitial.push(initialsInput.value);
+      userScores = userScores.concat(userInitial);
+      userTag = userScores[0] + userScores[2];
+      console.log(userTag);
+    } else {
+      alert("Must input Initials");
+    }
+  });
+  storeInfo();
 }
+// function to send user score and intials to local storage.
+function storeInfo() {
+    localStorage.setItem('userTag', JSON.stringify(userTag));
+}
+// get scores form local storage to display
+function getScores() {
+    for (let i = 0; i < userTag.length; i++) {
+      const user = JSON.parse(localStorage.getItem("userTag"));
+      const userList = document.createElement("li");
+      userList.innerText = user[i];
+      $results.appendChild(userList);
+    }
+  }
+
 // function that runs when quiz is over.
 function gameOver() {
-    
-    $jumboHeader.textContent = "Quiz has ended"
-    $buttonArea.style.visibility = "hidden";
-    $questionArea.style.visibility = "hidden";
-    $timeLabel.textContent = "";
-    $results.textContent = "GAME OVER. \n Your final score is:  " + score;
-    timeLeft = 0;
-    enterScore();
+  $jumboHeader.textContent = "Quiz has ended";
+  $buttonArea.style.visibility = "hidden";
+  $questionArea.style.visibility = "hidden";
+  $timeLabel.textContent = "";
+  $results.textContent = "GAME OVER. \n Your final score is:  " + score;
+  userScores.push(score);
+  timeLeft = 0;
+  enterScore();
 }
-
